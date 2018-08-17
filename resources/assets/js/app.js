59229a0dfbc40364af5d90da82c4b930209c1cb0
ubuntu <ubuntu@qq.com>
2018-08-17 23:46:28 +0800

    1

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -38,7 +38,7 @@ Vue.use(ElementUI);
 // 2. 定义路由
 // 每个路由应该映射一个组件。 其中"component" 可以是
 // 通过 Vue.extend() 创建的组件构造器，
-// 或者，只是一个组件配置对象。
+// 或者，只是一个组件配置对象。asdasd
 // 我们晚点再讨论嵌套路由。
 
 const  IndexContent_ = () => Promise.resolve(IndexContent)
