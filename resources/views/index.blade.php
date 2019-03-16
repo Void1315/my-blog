6895d458a06f06d3512f27128b12d891b6ae2a73
asahi <381848900@qq.com>
2019-03-16 17:21:27 +0800

    https

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,7 +6,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 	<link rel="icon" href="{{ asset('img/icon.ico')}} " type="image/x-icon"/>
 	<link href="{{ asset('css/app.css') }}" rel="stylesheet">
-	
+	<meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
 	<link rel="stylesheet" type="text/css" href="https://unpkg.com/wangeditor@3.1.1/release/wangEditor.min.css">
 	<script src="https://cdn.bootcss.com/jquery/3.2.0/jquery.min.js"></script>
 </head>
@@ -14,7 +14,7 @@
 	<div id="app" style="height: 100%;">
 	  <router-view></router-view>
 	</div>
-	
+
 	<script src="https://cdn.bootcss.com/vue/2.5.7/vue.min.js"></script>
 	<script src="https://unpkg.com/element-ui@2.4.6/lib/index.js"></script>
 	<script src="https://cdn.bootcss.com/vue-router/3.0.1/vue-router.js"></script>
