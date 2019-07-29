fe3ad1ff666c8e0aa357045e51b663769cddf81b
whitevoid <381848900@qq.com>
2019-07-30 00:03:16 +0800

    修改cdn 与忽略文件

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,22 +6,22 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 	<meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
 	<link rel="icon" href="{{ asset('img/icon.ico')}} " type="image/x-icon"/>
-	<link href="{{ asset('css/app.css') }}" rel="stylesheet">
+	<link href="{{ mix('css/app.css') }}" rel="stylesheet">
 	
 	<link rel="preconnect" href="https://cdn.bootcss.com">
-	<link rel="stylesheet" type="text/css" href="https://unpkg.com/wangeditor@3.1.1/release/wangEditor.min.css">
+	<link href="https://cdn.bootcss.com/wangEditor/3.1.1/wangEditor.min.css" rel="stylesheet">
 	<link href="https://cdn.bootcss.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
 </head>
 <body>
 	<div id="app" style="height: 100%;">
 	  <router-view></router-view>
 	</div>
-	<script src="https://cdn.bootcss.com/vue/2.5.7/vue.min.js"></script>
-	<script src="https://unpkg.com/element-ui@2.4.11/lib/index.js"></script>
-	<script src="https://cdn.bootcss.com/vue-router/3.0.1/vue-router.js"></script>
-	<script src="https://cdn.bootcss.com/axios/0.18.0/axios.min.js"></script>
-	<script type="text/javascript" defer="defer" src="https://unpkg.com/wangeditor@3.1.1/release/wangEditor.min.js"></script>
+	<script src="https://cdn.bootcss.com/vue/2.6.10/vue.min.js"></script>
+	<script src="https://cdn.bootcss.com/element-ui/2.10.1/index.js"></script>
+	<script src="https://cdn.bootcss.com/vue-router/3.0.7/vue-router.js"></script>
+	<script src="https://cdn.bootcss.com/axios/0.19.0/axios.min.js"></script>
+	<script src="https://cdn.bootcss.com/wangEditor/3.1.1/wangEditor.min.js"></script>
 	<script src="https://cdn.bootcss.com/vue-lazyload/1.2.6/vue-lazyload.js"></script>
-	<script src="{{ asset('js/app.js') }}"></script>
+	<script src="{{ mix('js/app.js') }}"></script>
 </body>
 </html>
